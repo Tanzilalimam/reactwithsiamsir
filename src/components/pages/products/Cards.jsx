bc78bf144499cb4c34d89a3348e477c31e2ac1f2
Tanzilalimam <gittanzilal@gmail.com>
2026-09-14 19:46:00 +0600

    u

--- src/components/pages/products/Cards.jsx
+++ src/components/pages/products/Cards.jsx
@@ -1,16 +1,21 @@
+import axios from 'axios'
+import Img from '../../../ui/Img'
+import Para from '../../../ui/para'
+import Pbtn from '../../../ui/Pbtn'
+import Beef from '../../../assets/img/beeffilet.webp'
 
 const Cards = function(){
 
     return(
-        <section>
+        <section className='my-10'>
             <div className="container">
-                <div className="flex flex-wrap justify-center items-center gap-6">
-                    <div className="flex flex-col items-start gap-3">
-                    <div className="flex flex-col items-start gap-3">
-                        cards
-                    </div>
+                <div className="flex flex-wrap justify-center items-center gap-5">
+                    <div className="w-[25%] flex flex-col items-start gap-3 border-2 border-black rounded-t-xl">
+                    <Img src={Beef} alt='no pic' className='w-full object-cover rounded-t-xl'></Img>
                     <div className="px-6 flex flex-col items-start gap-3">
-                        cards
+                        <h4 className='text-xl font-semibold capitalize'>beef filet (premium)</h4>
+                        <Para className='' text={'kjhjghahgkajhgkaghrghgguh'}></Para>
+                        <Pbtn className='mb-5 bg-blue-500 text-white/75 hover:text-white capitalize' text='learn more'></Pbtn>
                     </div>
                     </div>
                 </div>
